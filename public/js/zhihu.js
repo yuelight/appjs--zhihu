7a6b592210ef61ae0bad76fd03ad9b5e318a3fa2
yuelight <vaeqfe@gmail.com>
2017-01-12 15:18:35 +0800

    proxy zhihu api

---
+++ public/js/zhihu.js
@@ -0,0 +1,13 @@
+App.controller('home', function(page) {
+    // put stuff here
+});
+
+App.controller('page2', function(page) {
+    // put stuff here
+});
+
+try {
+    App.restore();
+} catch (err) {
+    App.load('home');
+}
