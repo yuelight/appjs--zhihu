0a52a86ff30aac6196cf67b37dc8420d06a8c55d
yuelight <vaeqfe@gmail.com>
2017-01-14 13:54:06 +0800

    post list && image proxy

--- public/js/zhihu.js
+++ public/js/zhihu.js
@@ -1,5 +1,31 @@
+// 微型模板引擎，不支持循环
+$.fn.template = function (data) {
+    var template = $(this[0]).html().trim();
+
+    if (typeof data === "object") {
+        for (var key in data) {
+            template = template.replace(new RegExp('\\${' + key + '}', 'g'), data[key]);
+        }
+    }
+    return template;
+};
+
 App.controller('home', function(page) {
-    // put stuff here
+    $container = $(page).find('#js-story-container');
+    $template = $(page).find('#js-story-template');
+
+    $.getJSON('/api/4/news/latest', function (data) {
+        if (data.stories && data.stories.length) {
+            for (var i = 0, n = data.stories.length; i < n; i++) {
+                var tplData = {
+                    image: '/img/proxy?img=' + encodeURI(data.stories[i].images[0]),
+                    title: data.stories[i].title
+                };
+
+                $container.append($template.template(tplData));
+            }
+        }
+    });
 });
 
 App.controller('page2', function(page) {
